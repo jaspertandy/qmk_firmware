0f11e3a08abe1a7cbce5497b21c85ba773413dd3
jaspertandy <jspr@tndy.me>
2019-08-01 09:33:59 +0100

    Emoji grin

--- keyboards/keebio/levinson/keymaps/jaspertandy/keymap.c
+++ keyboards/keebio/levinson/keymaps/jaspertandy/keymap.c
@@ -42,6 +42,7 @@ enum custom_keycodes {
   EMOJI_THUMBSUP,
   EMOJI_SMIRK,
   EMOJI_UNAMUSED,
+  EMOJI_GRIN,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -96,13 +97,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  VIM_NOHLSEARCH, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  VIMUX_PROMPT, VIMUX_REPEAT, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  VIMUX_PROMPT, VIMUX_REPEAT, VIM_NOHLSEARCH, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * | Reset|      |      |      |      |      |🙄    👍  |  😏    |  😒    |      |      | R
+ * | Reset|      |      |      |      |      |🙄    👍  |  😏    |  😒    😁|         |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Aud On|      |      |      |      |      |      |      |      |      |      |Aud On|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -112,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  RESET, _______, _______, _______, _______, _______, EMOJI_ROLLEYES, EMOJI_THUMBSUP, EMOJI_SMIRK, EMOJI_UNAMUSED, _______, _______, \
+  RESET, _______, _______, _______, _______, _______, EMOJI_ROLLEYES, EMOJI_THUMBSUP, EMOJI_SMIRK, EMOJI_UNAMUSED, EMOJI_GRIN, _______, \
   AU_ON, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   AU_OFF, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, AG_SWAP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -292,6 +293,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case EMOJI_UNAMUSED:
       if (record->event.pressed) {
           SEND_STRING(SS_LALT("D83D+DE12"));
+      }
+      return false;
+      break;
+    case EMOJI_GRIN:
+      if (record->event.pressed) {
+          SEND_STRING(SS_LALT("D83D+DE01"));
       }
       return false;
       break;
