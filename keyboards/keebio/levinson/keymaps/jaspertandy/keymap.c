6af051022e146cab2b61770cf0767132dca08967
jaspertandy <jspr@tndy.me>
2019-09-12 13:59:21 +0100

    Remove emoji and that

--- keyboards/keebio/levinson/keymaps/jaspertandy/keymap.c
+++ keyboards/keebio/levinson/keymaps/jaspertandy/keymap.c
@@ -18,7 +18,6 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
-  FUCKING_POUND,
   TMUX_PANE_N,
   TMUX_WIN_N,
   TMUX_PANE_P,
@@ -35,15 +34,13 @@ enum custom_keycodes {
   TMUX_P3,
   TMUX_P4,
   TMUX_P5,
+  TMUX_D,
   VIMUX_PROMPT,
   VIMUX_REPEAT,
   VIM_NOHLSEARCH,
-  EMOJI_ROLLEYES,
-  EMOJI_THUMBSUP,
-  EMOJI_SMIRK,
-  EMOJI_UNAMUSED,
-  EMOJI_GRIN,
+  SCREENSHOT_COPY
 };
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -62,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  ADJUST,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
@@ -71,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |     |    \  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |   £  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | T>P  | T<P  | T>W  | T<W  |
  * `-----------------------------------------------------------------------------------'
@@ -79,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM,     KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_DEL,  TMUX_W0, TMUX_W1, TMUX_W2, TMUX_W3, TMUX_W4, TMUX_W5, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, TMUX_P0, TMUX_P1, TMUX_P2, TMUX_P3, TMUX_P4, TMUX_P5, S(KC_NUHS),S(KC_NUBS),_______, FUCKING_POUND, _______, \
+  _______, TMUX_P0, TMUX_P1, TMUX_P2, TMUX_P3, TMUX_P4, TMUX_P5, S(KC_NUHS),S(KC_NUBS),_______, LALT(KC_3), SCREENSHOT_COPY, \
   _______, _______, _______ ,_______, _______, _______, _______, _______, TMUX_PANE_P, TMUX_PANE_N, TMUX_WIN_P, TMUX_WIN_N \
 ),
 
@@ -98,12 +95,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  VIMUX_PROMPT, VIMUX_REPEAT, VIM_NOHLSEARCH, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  VIMUX_PROMPT, VIM_NOHLSEARCH, _______, VIMUX_REPEAT, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * | Reset|      |      |      |      |      |🙄    👍  |  😏    |  😒    😁|         |
+ * | Reset|      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Aud On|      |      |      |      |      |      |      |      |      |      |Aud On|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -113,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  RESET, _______, _______, _______, _______, _______, EMOJI_ROLLEYES, EMOJI_THUMBSUP, EMOJI_SMIRK, EMOJI_UNAMUSED, EMOJI_GRIN, _______, \
+  RESET, _______, _______, _______, _______, _______, TMUX_D, _______, _______, _______, _______, _______, \
   AU_ON, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   AU_OFF, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, AG_SWAP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -149,12 +146,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_ADJUST);
       } else {
         layer_off(_ADJUST);
-      }
-      return false;
-      break;
-    case FUCKING_POUND:
-      if (record->event.pressed) {
-          SEND_STRING(SS_LALT("00A3"));
       }
       return false;
       break;
@@ -254,6 +245,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case TMUX_D:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LCTRL("b")"d");
+      }
+      return false;
+      break;
     case VIMUX_PROMPT:
       if (record->event.pressed) {
         SEND_STRING("\\vp");
@@ -272,33 +269,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case EMOJI_ROLLEYES:
+    case SCREENSHOT_COPY:
       if (record->event.pressed) {
-          SEND_STRING(SS_LALT("D83D+DE44"));
-      }
-      return false;
-      break;
-    case EMOJI_THUMBSUP:
-      if (record->event.pressed) {
-          SEND_STRING(SS_LALT("D83D+DC4D"));
-      }
-      return false;
-      break;
-    case EMOJI_SMIRK:
-      if (record->event.pressed) {
-          SEND_STRING(SS_LALT("D83D+DE0F"));
-      }
-      return false;
-      break;
-    case EMOJI_UNAMUSED:
-      if (record->event.pressed) {
-          SEND_STRING(SS_LALT("D83D+DE12"));
-      }
-      return false;
-      break;
-    case EMOJI_GRIN:
-      if (record->event.pressed) {
-          SEND_STRING(SS_LALT("D83D+DE01"));
+          SEND_STRING(SS_LCTRL(SS_LSFT(SS_LGUI("4"))));
       }
       return false;
       break;
