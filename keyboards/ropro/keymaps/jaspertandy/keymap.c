35b2af7a671afe07639eed836f6cffe3b508ec57
jaspertandy <jspr@tndy.me>
2019-10-09 11:45:28 +0100

    Add a tilde that's easier to access

--- keyboards/ropro/keymaps/jaspertandy/keymap.c
+++ keyboards/ropro/keymaps/jaspertandy/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        |+------+------+------+------+-----+------+------+------+------+------+------+------|--------------.
    *        | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |  Play/Pause  |
    * ,------+------+------+------+------+------+------+------+------+------+------+------+------|--------------'
-   * | \vr  | Raise|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | \vr  | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------|
    * | \vp  | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_GRAVE  , KC_F2    , KC_F3   , KC_F4   , KC_F5 , KC_F6  , KC_F7  , KC_F8 , KC_LBRC , KC_RBRC , KC_MINUS , KC_EQUAL ,
                      KC_ESC    , KC_1     , KC_2    , KC_3    , KC_4  , KC_5   , KC_6   , KC_7  , KC_8    , KC_9    , KC_0     , KC_BSPC  ,
     KC_MPLY        , KC_TAB    , KC_Q     , KC_W    , KC_E    , KC_R  , KC_T   , KC_Y   , KC_U  , KC_I    , KC_O    , KC_P     , KC_BSLS  ,
-    VIMUX_REPEAT   , RAISE     , KC_A     , KC_S    , KC_D    , KC_F  , KC_G   , KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN  , KC_QUOT  ,
+    VIMUX_REPEAT   , KC_ESC    , KC_A     , KC_S    , KC_D    , KC_F  , KC_G   , KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN  , KC_QUOT  ,
     VIMUX_PROMPT   , KC_LSHIFT , KC_Z     , KC_X    , KC_C    , KC_V  , KC_B   , KC_N   , KC_M  , KC_COMM , KC_DOT  , KC_SLSH  , KC_ENT   ,
     VIM_ACK        , KC_LCTRL  , KC_RCTRL , KC_LALT , KC_LGUI , LOWER , KC_SPC , KC_SPC , RAISE , KC_LEFT , KC_DOWN , KC_UP    , KC_RGHT
   ),
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        ,-----------------------------------------------------------------------------------.
    *        |TOGRGB|      |      |      |Sat(-)|Hue(-)|Hue(+)|Sat(+)|      |      |Brght-|Brght+|
    *        |------+------+------+------+------+-------------+------+------+------+------+------|
-   *        |      |      |      |      |      |      |      |   7  |   8  |   9  |      |      |
+   *        |      |      |      |      |      |      |      |   7  |   8  |   9  |      |  `   |
    *        |+------+------+------+------+-----+------+------+------+------+------+------+------|--------------.
    *        |      |      |  Up  |      |      |      |      |   4  |   5  |   6  |      |      |    c-B D     |
    * ,------+------+------+------+------+------+------+------+------+------+------+------+------|--------------'
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
     [_LOWER] = LAYOUT(
                       RGB_TOG , KC_TRNS , KC_TRNS , KC_TRNS , RGB_SAD , RGB_HUD , RGB_HUI , RGB_SAI , KC_TRNS , KC_TRNS , RGB_VAD , RGB_VAI         ,
-                      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_7    , KC_8    , KC_9    , KC_TRNS , KC_TRNS         ,
+                      KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_7    , KC_8    , KC_9    , KC_TRNS , KC_TILD         ,
     TMUX_D          , KC_TRNS , KC_TRNS , KC_UP   , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_4    , KC_5    , KC_6    , KC_TRNS , KC_TRNS         ,
     KC_PGDN         , KC_TRNS , KC_LEFT , KC_DOWN , KC_RGHT , KC_TRNS , KC_TRNS , KC_TRNS , KC_1    , KC_2    , KC_3    , KC_TRNS , KC_TRNS         ,
     COLOR_PICKER    , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_0    , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS         ,
