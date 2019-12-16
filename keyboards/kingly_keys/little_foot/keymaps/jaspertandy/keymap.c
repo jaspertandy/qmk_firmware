d2fef2dd4d6d4b229b6404adf74d9874d8dfdf0c
jaspertandy <jspr@tndy.me>
2019-12-16 11:17:42 +0000

    Is alt dot?

--- keyboards/kingly_keys/little_foot/keymaps/jaspertandy/keymap.c
+++ keyboards/kingly_keys/little_foot/keymaps/jaspertandy/keymap.c
@@ -6,7 +6,7 @@
 #define FN MO(_FN)
 #define LN MO(_LN)
 #define SFT_SPC MT(MOD_LSFT , KC_SPC)
-#define ALT_C MT(MOD_LALT , KC_C)
+#define ALT_DOT MT(MOD_LALT , KC_C)
 #define xxx KC_TRNS
 
 enum {
@@ -92,10 +92,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_space_base(
-        KC_1    , KC_2       , KC_3       , KC_4    , KC_5 , KC_6 , KC_7 , KC_8    , KC_9   , KC_0    ,
-        KC_Q    , KC_W       , KC_E       , KC_R    , KC_T , KC_Y , KC_U , KC_I    , KC_O   , KC_P    ,
-        KC_A    , KC_S       , KC_D       , KC_F    , KC_G , KC_H , KC_J , KC_K    , KC_L   , FN      ,
-        KC_Z    , KC_X       , ALT_C       , KC_V    , KC_B , KC_N , KC_M , KC_COMM , KC_DOT , KC_SLSH ,
+        KC_1    , KC_2    , KC_3   , KC_4    , KC_5 , KC_6 , KC_7 , KC_8    , KC_9    , KC_0    ,
+        KC_Q    , KC_W    , KC_E   , KC_R    , KC_T , KC_Y , KC_U , KC_I    , KC_O    , KC_P    ,
+        KC_A    , KC_S    , KC_D   , KC_F    , KC_G , KC_H , KC_J , KC_K    , KC_L    , FN      ,
+        KC_Z    , KC_X    , KC_C   , KC_V    , KC_B , KC_N , KC_M , KC_COMM , ALT_DOT , KC_SLSH ,
         KC_LGUI , SFT_SPC , KC_SPC , KC_RCTL
     ),
 
