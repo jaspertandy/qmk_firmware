082c2d8c5ff8be07aea8f16d50ebd26aa726badf
jaspertandy <jspr@tndy.me>
2020-02-29 17:45:29 +0000

    Swap layout to be more handsy

--- keyboards/kyria/keymaps/jaspertandy/keymap.c
+++ keyboards/kyria/keymaps/jaspertandy/keymap.c
@@ -35,57 +35,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Tab   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | Raise|Lower |  |Raise |Low/Up|   N  |   M  | ,  < | . >  | /  ? |  | \   |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | Raise|Space |  |Raise |Low/Up|   N  |   M  | ,  < | . >  | /  ? |  | \   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | Play | Ctrl | Alt  | GUI  |Space |  | Enter| Left | Down |Right | Play |
- *                        |      |      |      |      |      |  |      | GUI  | Alt  | Ctrl |      |
+ *                        |      |      |      |      |Raise |  | Lower| GUI  | Alt  | Ctrl |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_ESC,       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LT(_RAISE, KC_SPC),   LT(_LOWER, KC_SPC), LT(_LOWER, KC_SPC), LT(_RAISE, KC_UP), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_BSLS),
-              KC_MPLY, KC_LCTRL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, MT(MOD_LGUI, KC_LEFT), MT(MOD_LALT, KC_DOWN), MT(MOD_LCTL, KC_RGHT), KC_MPLY
+      KC_ESC,       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                                                                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                                                                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                  LT(_LOWER, KC_SPC),   KC_SPC,            LT(_LOWER, KC_SPC), LT(_RAISE, KC_UP),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_BSLS),
+                                            KC_MPLY, KC_LCTRL, KC_LALT, KC_LGUI, LT(_RAISE, KC_SPC),            LT(_LOWER, KC_ENT), MT(MOD_LGUI, KC_LEFT), MT(MOD_LALT, KC_DOWN), MT(MOD_LCTL, KC_RGHT), KC_MPLY
     ),
 /*
- * Lower Layer: Number keys, media, navigation
+ * Lower Layer: Numbers left hand
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |   1  |  2   |  3   |  4   |  5   |                              |  6   |  7   |  8   |  9   |  0   |        |
+ * |        |   1  |  2   |  3   |  4   |  5   |                              |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              |      |  _   |  +   |  {   |  }   |        |
+ * |        |   6  |  7   |  8   |  9   |  0   |                              |      |      |      |      |      |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
+ * |        |      |      |      |  +   |  _   |      |      |  |      |      |      |      |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |VolUp | Play |
+ *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_LOWER] = LAYOUT(
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
-      _______, _______, _______, _______, _______, _______,                                     _______, KC_UNDERSCORE, KC_PLUS, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        _______, _______, _______, _______, _______,    KC_DELETE,
+      _______, KC_6, KC_7, KC_8, KC_9, KC_0,                                     _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, KC_PLUS, KC_UNDERSCORE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 /*
- * Raise Layer: Symbols
+ * Raise Layer: Symbols right hand
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |   ~    |  !   |  @   |  £   |  $   |  %   |                              |   ^  |   &  |  *   |  (   |  )   |  Del   |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |  #   |      |      |                              |   +  |  -   |  =   |  [   |  ]   |        |
+ * |        |      |      |      |      |      |                              |   !   |  @   |  £   |  $   |  %  |   ~    |
+ * ,-------------------------------------------.                              ,-------------------------------------------.
+ * |        |      |      |      |      |      |                              |   ^  |   &  |  *   |  (   |  )   |   `    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
+ * |        |      |      |      |      |      |      |      |  |      |  #   |   -  |  =   |  <   |  >   |  ?   |        |
+ * |--------+------+------+------+------+------+------+------|  |------+------+------+------+------+------+------+--------|
+ *                        |      |      |      |      |      |  |  {   |   }  |   [  |  ]   |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_RAISE] = LAYOUT(
-      KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT,                          KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-      _______, _______, _______, FORCE_HASH, _______, _______,                                     _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,   KC_LT,   KC_GT,   _______,  _______,
-               _______, _______, _______, _______, _______,                                     _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
+      _______, _______, _______, _______, _______, _______,                           KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_QUES,
+      _______, _______, _______, _______, _______, _______,                           KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_GRV,
+      _______, _______, _______, _______, _______, _______, _______, _______,   _______, FORCE_HASH, KC_MINS, KC_EQL,  KC_LT,   KC_GT, _______, KC_TILDE,
+               _______, _______, _______, _______, _______,                                     KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_LBRC, KC_RBRC, KC_MPLY
     ),
 /*
  * Adjust Layer: Function keys, RGB
@@ -252,14 +252,14 @@ static void render_status(void) {
             oled_write_P(PSTR("Rotary R: tmux pane\n"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("`123_+{}\n"), false);
-            oled_write_P(PSTR("Rotary L: Vol\n"), false);
-            oled_write_P(PSTR("Rotary R: tmux window\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("~!@$-=[]\n"), false);
+            oled_write_P(PSTR("Numbers LH\n"), false);
             oled_write_P(PSTR("Rotary L: Vol\n"), false);
             oled_write_P(PSTR("Rotary R: tmux pane\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("Symbols RH\n"), false);
+            oled_write_P(PSTR("Rotary L: Vol\n"), false);
+            oled_write_P(PSTR("Rotary R: tmux window\n"), false);
             break;
         case _ADJUST:
             oled_write_P(PSTR("F1\n"), false);
@@ -291,7 +291,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
     else if (index == 1) {
-        if (IS_LAYER_ON(_LOWER)) {
+        if (IS_LAYER_ON(_RAISE)) {
             if (clockwise) {
                 // Next tmux window
                 SEND_STRING(SS_LCTRL("b")"n");
