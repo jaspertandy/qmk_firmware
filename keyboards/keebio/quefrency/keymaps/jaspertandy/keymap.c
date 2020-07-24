1e932c4a0973c920f01375ff19414610cc6e607c
jaspertandy <jspr@tndy.me>
2020-07-24 17:50:36 +0100

    Some personalisation

--- keyboards/keebio/quefrency/keymaps/jaspertandy/keymap.c
+++ keyboards/keebio/quefrency/keymaps/jaspertandy/keymap.c
@@ -11,14 +11,20 @@ extern keymap_config_t keymap_config;
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  SCREENSHOT_COPY,
+  SCREENSHOT_SCROLLING,
+  SCREENSHOT_RECORD,
+  VIMUX_PROMPT,
+  VIMUX_REPEAT,
+  COLOR_PICKER
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_F1,   KC_F2,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME, \
-    KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_F5,   KC_F6,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_MPLY, KC_GRV,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_MNXT, \
+    VIMUX_PROMPT,   VIMUX_REPEAT,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, SCREENSHOT_COPY, \
+    COLOR_PICKER,   KC_F6,   KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  SCREENSHOT_SCROLLING, \
+    KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   SCREENSHOT_RECORD, \
     KC_F9,   KC_F10,  KC_LCTL, KC_LALT, KC_LGUI, MO(_FN1),KC_SPC,           KC_ENT,  KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -34,16 +40,69 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
         }
+    } else if (index == 1) {
+        if (IS_LAYER_ON(_FN1)) {
+            if (clockwise) {
+                // Next tmux window
+                SEND_STRING(SS_LCTRL("b")"n");
+            } else {
+                // Previous tmux window
+                SEND_STRING(SS_LCTRL("b")"m");
+            }
+        } else {
+            if (clockwise) {
+                // Next tmux pane
+                SEND_STRING(SS_LCTRL("b")"o");
+            } else {
+                // Previous tmux pane
+                SEND_STRING(SS_LCTRL("b")"p");
+            }
+        }
     }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case SCREENSHOT_COPY:
+      if (record->event.pressed) {
+          SEND_STRING(SS_LSFT(SS_LGUI("4")));
+      }
+      return false;
+      break;
+    case SCREENSHOT_SCROLLING:
+      if (record->event.pressed) {
+          SEND_STRING(SS_LSFT(SS_LGUI("5")));
+      }
+      return false;
+      break;
+    case SCREENSHOT_RECORD:
+      if (record->event.pressed) {
+          SEND_STRING(SS_LSFT(SS_LGUI("1")));
+      }
+      return false;
+      break;
+    case VIMUX_PROMPT:
+      if (record->event.pressed) {
+          SEND_STRING("\\vp");
+      }
+      return false;
+      break;
+    case VIMUX_REPEAT:
+      if (record->event.pressed) {
+          SEND_STRING(SS_TAP(X_ESCAPE)"\\r");
+      }
+      return false;
+      break;
+    case COLOR_PICKER:
+      if (record->event.pressed) {
+          SEND_STRING(SS_LCTRL(SS_LSFT(SS_LALT("c"))));
+      }
+      break;
+  }
+
+  return true;
 }
