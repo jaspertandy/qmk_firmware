2d5412cb73e6075b5dd0ea04b959ab1d4f408638
jaspertandy <jspr@tndy.me>
2020-10-02 21:34:16 +0100

    Better tapping config for Sofle

--- keyboards/sofle/keymaps/jaspertandy/config.h
+++ keyboards/sofle/keymaps/jaspertandy/config.h
@@ -4,3 +4,8 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+#define IGNORE_MOD_TAP_INTERRUPT
+#define RETRO_TAPPING
+#undef PERMISSIVE_HOLD
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
