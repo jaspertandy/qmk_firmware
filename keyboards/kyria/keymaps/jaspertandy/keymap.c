ef2bf770956ba1208765614918f8a62be641c3f9
jaspertandy <jspr@tndy.me>
2020-10-13 18:01:21 +0100

    Update Kyria to match my Corne plans

--- keyboards/kyria/keymaps/jaspertandy/keymap.c
+++ keyboards/kyria/keymaps/jaspertandy/keymap.c
@@ -32,18 +32,6 @@ enum custom_keycodes {
   PASSWORD1
 };
 
-//Tap Dance Declarations
-enum {
-  TD_LPARENS = 0,
-  TD_RPARENS
-};
-
-// Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LPARENS]  = ACTION_TAP_DANCE_DOUBLE(KC_LEFT_PAREN, KC_LBRC),
-  [TD_RPARENS]  = ACTION_TAP_DANCE_DOUBLE(KC_RIGHT_PAREN, KC_RBRC)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
@@ -53,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Tab   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |   ([   |   Z  |   X  |   C  |   V  |   B  | Caps | LSft |  | LSft |Rai/Up|   N  |   M  | ,  < | . >  | /  ? |   )]   |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Play | Ctrl | Alt  | GUI  |Space |  | Enter| Left | Down |Right | Play |
- *                        |      |      |      |      |      |  |      | GUI  | Alt  | Ctrl |      |
+ * |   ([   |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |   )]   |
+ * `----------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
+ *                        | Play | Ctrl | GUI  |Lower |Space |  | Enter|Raise |      |      | Play |
+ *                        |      |      |      |      |      |  |      |      | GUI  | Alt  |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_LALT, LT(_ADJUST, KC_BSLS),            KC_BSLS, MT(MOD_RALT, KC_UP),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                            PASSWORD1, KC_LCTRL, KC_LGUI, MO(_RAISE), KC_SPC,         KC_ENT, LT(_LOWER, KC_LEFT), MT(MOD_RGUI, KC_DOWN), MT(MOD_RCTL, KC_RGHT), KC_MPLY
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______, _______,            _______, _______,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                            PASSWORD1, KC_LCTRL, KC_LGUI, MO(_LOWER), KC_SPC,         KC_ENT, MO(_RAISE), KC_RGUI, KC_RALT, KC_MPLY
     ),
 /*
- * Lower Layer: Numbers left hand
+ * Raise Layer: Numbers left hand
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |        |  +   |  *   |  1   |  2   |  3   |                              |      |  (   |   )  |      |      |   Del  |
@@ -74,19 +62,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |  -   |  /   |  4   |  5   |  6   |                              |      |  {   |  }   |      |      |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |  =   |  =   |  7   |  8   |  9   |      |      |  |      |      |   \  |  [   |  ]   |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |  0   |   _  |  |      |xxxxxx|      |      |      |
+ * `----------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
+ *                        |      |      |  0   |      |      |  |      |xxxxxx|      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_LOWER] = LAYOUT(
-      _______, KC_PLUS, KC_1,    KC_2,        KC_3,    KC_UNDERSCORE,                                         _______, KC_LPRN, KC_RPRN, _______, _______, KC_DELETE,
-      _______, KC_MINS, KC_4,    KC_5,        KC_6,    KC_MINS,                                               _______, KC_LCBR, KC_RCBR, _______, _______, _______,
-      _______, KC_EQL , KC_7,    KC_8,        KC_9,    KC_EQL, _______, KC_UNDERSCORE,      _______, _______, KC_BSLS, KC_LBRC, KC_RBRC, _______, _______, _______,
-                                 KC_ASTERISK, KC_SLSH, KC_0, _______, KC_MINS,              _______, _______, _______, _______, _______
+    [_RAISE] = LAYOUT(
+      _______, KC_PLUS,     KC_1,    KC_2,        KC_3, KC_UNDERSCORE,                                    _______, KC_LPRN, KC_RPRN, _______, _______, KC_DELETE,
+      _______, KC_ASTERISK, KC_4,    KC_5,        KC_6, KC_MINS,                                          KC_PIPE, KC_LCBR, KC_RCBR, _______, _______, _______,
+      _______, KC_SLSH ,    KC_7,    KC_8,        KC_9, KC_EQL,  _______, _______,      _______, _______, KC_BSLS, KC_LBRC, KC_RBRC, _______, _______, _______,
+                                         _______, _______, KC_0, _______, _______,      _______, _______, _______, _______, _______
     ),
 /*
- * Raise Layer: Symbols right hand
+ * Lower Layer: Symbols right hand
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |        |      | :Ack | \vp  |      |      |                              |   !   |  @   |  ?   |  $   |     |   ~    |
@@ -99,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_RAISE] = LAYOUT(
-      _______, _______, VIM_ACK, VIMUX_PROMPT, _______, KC_CAPS,                           KC_EXCLAIM,  KC_AT,  KC_QUES,  KC_DOLLAR, _______, KC_TILDE,
-      _______, _______, COLOR_PICKER, VIMUX_REPEAT, _______, _______,                           KC_CIRCUMFLEX, KC_PERCENT, KC_ASTERISK, KC_AMPERSAND, KC_HASH, KC_GRV,
-      _______, _______, _______, SCREENSHOT_COPY, _______, _______, _______, _______,   KC_UNDERSCORE, FORCE_HASH, KC_BSLS, KC_PIPE,  KC_LT,   KC_GT, KC_EQL, _______,
-               _______, _______, _______, _______, _______,                                     KC_MINS, KC_UNDERSCORE, _______, _______, KC_MPLY
+    [_LOWER] = LAYOUT(
+      _______, KC_EXCLAIM, KC_AT,   KC_HASH,    KC_DOLLAR,    KC_PERCENT,                                         KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_MINS,  KC_UNDERSCORE, KC_EQL,
+      _______, _______,    _______, FORCE_HASH, _______,      _______,                                            KC_LEFT,       KC_DOWN,      KC_UP,       KC_RIGHT, _______,       KC_TILDE,
+      _______, _______,    _______, _______,    VIMUX_PROMPT, VIMUX_REPEAT, _______, _______,   _______, _______, _______,       _______,      KC_LT,       KC_GT,    _______,       KC_GRV,
+                                    _______,    _______,      _______,      _______, _______,   _______, _______, _______,       _______,      _______
     ),
 /*
  * Adjust Layer: Function keys, RGB
@@ -146,6 +134,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 //     ),
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -265,28 +257,39 @@ void oled_task_user(void) {
 
 #ifdef ENCODER_ENABLE
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (IS_LAYER_ON(_ADJUST)) {
-        if (clockwise) {
-            // Next tmux window
-            SEND_STRING(SS_LCTRL("b")"n");
+    if (index == 0) {
+        if (IS_LAYER_ON(_RAISE)) {
+            if (clockwise) {
+                // Next tmux window
+                SEND_STRING(SS_LCTRL("b")"n");
+            } else {
+                // Previous tmux window
+                SEND_STRING(SS_LCTRL("b")"m");
+            }
         } else {
-            // Previous tmux window
-            SEND_STRING(SS_LCTRL("b")"m");
+            if (clockwise) {
+                // Next tmux pane
+                SEND_STRING(SS_LCTRL("b")"o");
+            } else {
+                // Previous tmux pane
+                SEND_STRING(SS_LCTRL("b")"p");
+            }
         }
-    } else if (IS_LAYER_ON(_RAISE)) {
-        if (clockwise) {
-            // Next tmux pane
-            SEND_STRING(SS_LCTRL("b")"o");
+    } else if (index == 1) {
+         if (IS_LAYER_ON(_LOWER)) {
+            if (clockwise) {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_RGHT)));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_LEFT)));
+            }
         } else {
-            // Previous tmux pane
-            SEND_STRING(SS_LCTRL("b")"p");
-        }
-    } else {
-        // Volume control
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
+            // Volume control
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+
         }
     }
 }
