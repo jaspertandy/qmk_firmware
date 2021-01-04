3c173c9f4d2b8eddef1f1c23180d9b5670c67845
jaspertandy <jspr@tndy.me>
2021-01-04 15:59:14 +0000

    Bottom row is never done

--- keyboards/whimsy/keymaps/default/keymap.c
+++ keyboards/whimsy/keymaps/default/keymap.c
@@ -1,14 +1,15 @@
 #include "whimsy.h"
 #define _______ KC_TRNS
 #define LT3_TAB LT(3, KC_TAB)
+#define LT2_ENT LT(2, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 KEYMAP(
     KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T   , KC_Y  , KC_U    , KC_I    , KC_O   , KC_P     , KC_BSPC ,
-    LT3_TAB , KC_A    , KC_S    , KC_D    , KC_F  , KC_G   , KC_H  , KC_J    , KC_K    , KC_L   , KC_SCLN  , KC_ENT  ,
-    KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B   , KC_N  , KC_M    , KC_COMM , KC_DOT , KC_SLSH  , KC_QUOT ,
-    KC_BSLS , KC_LCTL , KC_RALT , KC_LGUI , MO(1) , KC_SPC , MO(2) , KC_LEFT , KC_DOWN , KC_UP  , KC_RIGHT , KC_MPLY
+    LT3_TAB , KC_A    , KC_S    , KC_D    , KC_F  , KC_G   , KC_H  , KC_J    , KC_K    , KC_L   , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B   , KC_N  , KC_M    , KC_COMM , KC_DOT , KC_SLSH  , KC_GRV ,
+    KC_LCTL , KC_LALT , KC_LGUI , KC_BSLS , MO(1) , KC_SPC , LT2_ENT , KC_LEFT , KC_DOWN , KC_UP  , KC_RIGHT , KC_MPLY
 ),
 
 KEYMAP( /* LEFT */
