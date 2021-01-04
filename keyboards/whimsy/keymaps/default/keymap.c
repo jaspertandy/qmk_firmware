b81e882fc767c99a3df5cf9cd2bde386cd0785ac
jaspertandy <jspr@tndy.me>
2021-01-04 16:17:18 +0000

    There will be a place for this to go

--- keyboards/whimsy/keymaps/default/keymap.c
+++ keyboards/whimsy/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ KEYMAP(
     KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T   , KC_Y  , KC_U    , KC_I    , KC_O   , KC_P     , KC_BSPC ,
     LT3_TAB , KC_A    , KC_S    , KC_D    , KC_F  , KC_G   , KC_H  , KC_J    , KC_K    , KC_L   , KC_SCLN  , KC_QUOT  ,
     KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B   , KC_N  , KC_M    , KC_COMM , KC_DOT , KC_SLSH  , KC_GRV ,
-    KC_LCTL , KC_LALT , KC_LGUI , KC_BSLS , MO(1) , KC_SPC , LT2_ENT , KC_LEFT , KC_DOWN , KC_UP  , KC_RIGHT , KC_MPLY
+    KC_LCTL , KC_LALT , KC_BSLS , KC_LGUI , MO(1) , KC_SPC , LT2_ENT , KC_LEFT , KC_DOWN , KC_UP  , KC_RIGHT , KC_MPLY
 ),
 
 KEYMAP( /* LEFT */
