2fde47b07770dd7c20724c4b84a05b715b229c3f
jaspertandy <jspr@tndy.me>
2021-07-09 18:57:01 +0100

    I still need that left pinky shift

--- keyboards/kyria/keymaps/jaspertandy/keymap.c
+++ keyboards/kyria/keymaps/jaspertandy/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      _______,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_LSFT, _______,            _______, KC_RSFT,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, KC_LSFT, _______,            KC_RALT, KC_RSFT,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
                             PASSWORD1, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,         KC_ENT, MO(_RAISE), KC_LGUI, KC_LALT, KC_MPLY
     ),
     [_LOWER] = LAYOUT(
